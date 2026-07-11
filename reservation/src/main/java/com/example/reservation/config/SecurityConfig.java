a8543e0518315d24b41a925c53f3dda246df3884
Georgiy Grobovoy <gro-jorge@mail.ru>
2026-07-11 13:31:31 +0300

    swagger-ui

--- reservation/src/main/java/com/example/reservation/config/SecurityConfig.java
+++ reservation/src/main/java/com/example/reservation/config/SecurityConfig.java
@@ -25,6 +25,8 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/event").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/event/{id}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/event/{id}/reserve").hasRole("ADMIN")
