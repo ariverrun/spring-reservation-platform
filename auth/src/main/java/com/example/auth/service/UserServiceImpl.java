92f8de9aa68589585324fc1ef2b33b5fc72182d8
Georgiy Grobovoy <gro-jorge@mail.ru>
2026-07-18 14:23:58 +0300

    publish user registered event

--- auth/src/main/java/com/example/auth/service/UserServiceImpl.java
+++ auth/src/main/java/com/example/auth/service/UserServiceImpl.java
@@ -4,16 +4,21 @@ import com.example.auth.dto.RegisterRequestDto;
 import com.example.auth.dto.UserResponseDto;
 import com.example.auth.entity.Role;
 import com.example.auth.entity.User;
+import com.example.auth.event.UserRegisteredEvent;
 import com.example.auth.exceptions.EmailAlreadyUsedException;
 import com.example.auth.repository.RoleRepository;
 import com.example.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.Set;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -21,6 +26,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Override
     @Transactional
@@ -42,8 +48,18 @@ public class UserServiceImpl implements UserService {
 
         User savedUser = userRepository.save(user);
 
-        return new UserResponseDto(
-            savedUser.getId()
+        eventPublisher.publishEvent(
+            new UserRegisteredEvent(
+                savedUser.getId(),
+                savedUser.getEmail(),
+                savedUser.getFirstName(),
+                savedUser.getLastName(),
+                Instant.now()
+            )
         );
+
+        log.info("User registered with id: {}", savedUser.getId());
+
+        return new UserResponseDto(savedUser.getId());
     }
 }
