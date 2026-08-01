9e8d4a05a43407e69e5aa983614c98539328982c
Georgiy Grobovoy <gro-jorge@mail.ru>
2026-08-01 18:53:09 +0300

    fixed security for openapi in reserves

--- reservation/src/main/java/com/example/reservation/config/SecurityConfig.java
+++ reservation/src/main/java/com/example/reservation/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,28 +22,34 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(AbstractHttpConfigurer::disable)
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/v3/api-docs**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/v1/event").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/v1/event/my").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/api/v1/event/{id}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/v1/event/{id}/reserve").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.POST, "/api/v1/event").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.PUT, "/api/v1/event/{id}").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/event/{id}").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.POST, "/api/v1/reserve").hasRole("USER")
-                .requestMatchers(HttpMethod.GET, "/api/v1/reserve").hasRole("USER")
-                .requestMatchers(HttpMethod.GET, "/api/v1/reserve/{id}").hasRole("USER")
-                .requestMatchers(HttpMethod.PUT, "/api/v1/reserve/{id}").hasRole("USER")
-                .anyRequest().authenticated()
-            )
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(this::configureAuthorization)
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
+    return http.build();
+}
+
+    private void configureAuthorization(
+        AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth
+    ) {
+        auth
+            .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/v3/api-docs/*").permitAll()
+            .requestMatchers(HttpMethod.GET, "/v3/api-docs**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/v1/event").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/v1/event/my").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.GET, "/api/v1/event/{id}").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/v1/event/{id}/reserve").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.POST, "/api/v1/event").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.PUT, "/api/v1/event/{id}").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.DELETE, "/api/v1/event/{id}").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.POST, "/api/v1/reserve").hasRole("USER")
+            .requestMatchers(HttpMethod.GET, "/api/v1/reserve").hasRole("USER")
+            .requestMatchers(HttpMethod.GET, "/api/v1/reserve/{id}").hasRole("USER")
+            .requestMatchers(HttpMethod.PUT, "/api/v1/reserve/{id}").hasRole("USER")
+            .anyRequest().authenticated();
     }
 }
